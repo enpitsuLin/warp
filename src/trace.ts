a74766276bedf8fbf5f5b67c854c9565dde5cee5
enpitsulin <enpitsulin@gmail.com>
2024-08-10 23:51:21 +0800

    fix: space

--- src/trace.ts
+++ src/trace.ts
@@ -1,7 +1,7 @@
 import { execSync } from 'node:child_process'
 
 export function fetchTrace(proxy?: string | false) {
-  const command = `curl https://www.cloudflare.com/cdn-cgi/trace/${proxy ? `-x ${proxy}` : ""}`
+  const command = `curl https://www.cloudflare.com/cdn-cgi/trace/${proxy ? ` -x ${proxy}` : ""}`
   const text = execSync(command).toString()
   const lines = text.split('\n');
   const dataObject: Record<string, string> = {};
